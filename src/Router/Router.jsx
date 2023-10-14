bfbd3e5816acdcd4e57b1bd4406b9ed725d0d488
JannatunNimeNishat <nishatnime100@gmail.com>
2023-10-14 18:05:12 +0600

    route setup and left navigation bar added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Dashboard />
+    }
+])
+
+
+
+export default router;
